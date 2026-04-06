08454e8824b5d705712501a9d66db720196ae600
divyanshv123 <divyanshv9114@gmail.com>
2026-04-06 18:57:06 +0530

    Fix UI font text colors and enable template auto-reload

--- app.py
+++ app.py
@@ -601,4 +601,4 @@ if __name__ == "__main__":
         os.makedirs(app.config["UPLOAD_FOLDER"])
         
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
