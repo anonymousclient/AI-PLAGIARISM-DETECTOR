ce253419dcf9b0de9ab46cdaee98f6ae682e08c9
divyanshv123 <divyanshv9114@gmail.com>
2026-04-07 07:14:40 +0530

    Cleanup: remove final temporary script

--- tmp_find_student.py
+++
@@ -1,8 +0,0 @@
-from db import users_collection
-
-student = users_collection.find_one({"role": "student"})
-if student:
-    print(f"Email: {student['email']}")
-    print(f"Password: {student['password']}")
-else:
-    print("No student user found.")
